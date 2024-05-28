1496259bfc8acac051d774f23443326d4ec98e04
huiquanjiang <huiquanjiang@microsoft.com>
2024-05-28 10:54:11 +0800

    add cloudtrail sdk

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 // NuGet packages: AWSSDK.S3, AWSSDK.SecurityToken, AWSSDK.SSO, AWSSDK.SSOOIDC
@@ -8,6 +9,8 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.SecurityToken;
 using Amazon.SecurityToken.Model;
+using Amazon.CloudTrail;
+using Amazon.CloudTrail.Model;
 
 namespace S3CreateAndList
 {
@@ -28,74 +31,17 @@ namespace S3CreateAndList
         {
             // Get SSO credentials from the information in the shared config file.
             // For this tutorial, the information is in the [default] profile.
-            var ssoCreds = LoadSsoCredentials("default");
+            var ssoCreds = LoadSsoCredentials("huiquan");
 
             // Display the caller's identity.
             var ssoProfileClient = new AmazonSecurityTokenServiceClient(ssoCreds);
-            Console.WriteLine($"\nSSO Profile:\n {await ssoProfileClient.GetCallerIdentityArn()}");
+            // Console.WriteLine($"\nSSO Profile:\n {await ssoProfileClient.GetCallerIdentityArn()}");
 
-            // Create the S3 client is by using the SSO credentials obtained earlier.
-            var s3Client = new AmazonS3Client(ssoCreds);
-
-            // Parse the command line arguments for the bucket name.
-            if (GetBucketName(args, out String bucketName))
-            {
-                // If a bucket name was supplied, create the bucket.
-                // Call the API method directly
-                try
-                {
-                    Console.WriteLine($"\nCreating bucket {bucketName}...");
-                    var createResponse = await s3Client.PutBucketAsync(bucketName);
-                    Console.WriteLine($"Result: {createResponse.HttpStatusCode.ToString()}");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Caught exception when creating a bucket:");
-                    Console.WriteLine(e.Message);
-                }
-            }
-
-            // Display a list of the account's S3 buckets.
-            Console.WriteLine("\nGetting a list of your buckets...");
-            var listResponse = await s3Client.ListBucketsAsync();
-            Console.WriteLine($"Number of buckets: {listResponse.Buckets.Count}");
-            foreach (S3Bucket b in listResponse.Buckets)
-            {
-                Console.WriteLine(b.BucketName);
-            }
-            Console.WriteLine();
+            var client = new AmazonCloudTrailClient(ssoCreds);
+            var response = await client.GetEventSelectorsAsync(new GetEventSelectorsRequest() { TrailName = "hqop17trail" });
+            Console.WriteLine($"{JsonSerializer.Serialize(response)}");
+            return;
         }
-
-        // 
-        // Method to parse the command line.
-        private static Boolean GetBucketName(string[] args, out String bucketName)
-        {
-            Boolean retval = false;
-            bucketName = String.Empty;
-            if (args.Length == 0)
-            {
-                Console.WriteLine("\nNo arguments specified. Will simply list your Amazon S3 buckets." +
-                  "\nIf you wish to create a bucket, supply a valid, globally unique bucket name.");
-                bucketName = String.Empty;
-                retval = false;
-            }
-            else if (args.Length == 1)
-            {
-                bucketName = args[0];
-                retval = true;
-            }
-            else
-            {
-                Console.WriteLine("\nToo many arguments specified." +
-                  "\n\ndotnet_tutorials - A utility to list your Amazon S3 buckets and optionally create a new one." +
-                  "\n\nUsage: S3CreateAndList [bucket_name]" +
-                  "\n - bucket_name: A valid, globally unique bucket name." +
-                  "\n - If bucket_name isn't supplied, this utility simply lists your buckets.");
-                Environment.Exit(1);
-            }
-            return retval;
-        }
-
         //
         // Method to get SSO credentials from the information in the shared config file.
         static AWSCredentials LoadSsoCredentials(string profile)
